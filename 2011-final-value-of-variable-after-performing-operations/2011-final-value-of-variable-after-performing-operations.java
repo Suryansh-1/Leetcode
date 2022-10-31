957b44eacdd049d32c2efbfebfeae837654c53ad
Suryansh-1 <116718977+Suryansh-1@users.noreply.github.com>
2022-10-31 21:45:46 +0530

    Time: 5 ms (13.70%), Space: 43.4 MB (34.88%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -3,11 +3,11 @@ class Solution {
         int x=0;
         for(int i=0;i<operations.length;i++)
         {
-            String s=operations[i];
-            if(s.equalsIgnoreCase("++X")||s.equalsIgnoreCase("X++"))
+            
+            if(operations[i].equalsIgnoreCase("++X")||operations[i].equalsIgnoreCase("X++"))
             
                 x=x+1;
-             if(s.equalsIgnoreCase("--X")||s.equalsIgnoreCase("X--"))
+             if(operations[i].equalsIgnoreCase("--X")||operations[i].equalsIgnoreCase("X--"))
                 x=x-1;
         }
         return x;
